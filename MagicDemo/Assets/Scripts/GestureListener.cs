5165907c50b999895012082e25f786e88c18caed
eugene.lagutkin <eugene.lagutkin@gmail.com>
2017-09-23 14:47:58 +0300

    Multiple effects for drawing now supported

--- MagicDemo/Assets/Scripts/GestureListener.cs
+++ MagicDemo/Assets/Scripts/GestureListener.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Valve.VR.InteractionSystem;
 
 [RequireComponent(typeof(Hand))]
@@ -20,7 +18,7 @@ public class GestureListener : MonoBehaviour {
 	}
 
 	[SerializeField]
-	private ParticleSystem m_drawingIndicator;
+	private ParticleSystem[] m_drawingIndicator;
 	[SerializeField]
 	private Animator m_handAnimator;
 
@@ -29,7 +27,9 @@ public class GestureListener : MonoBehaviour {
 	private GestureListenerState m_state;
 
 	protected void Awake() {
-		m_drawingIndicator.Stop();
+		foreach (ParticleSystem system in m_drawingIndicator) {
+			system.Stop();
+		}
 		m_handAnimator.SetBool("IsTriggered", false);
 		m_state = GestureListenerState.Idle;
 	}
@@ -38,14 +38,18 @@ public class GestureListener : MonoBehaviour {
 		switch (m_state) {
 			case GestureListenerState.Idle:
 				if (MyHand.hoveringInteractable == null && MyHand.GetStandardInteractionButtonDown()) {
-					m_drawingIndicator.Play();
+					foreach (ParticleSystem system in m_drawingIndicator) {
+						system.Play();
+					}
 					m_handAnimator.SetBool("IsTriggered", true);
 					m_state = GestureListenerState.Listening;
 				}
 				break;
 			case GestureListenerState.Listening:
 				if (MyHand.GetStandardInteractionButtonUp()) {
-					m_drawingIndicator.Stop();
+					foreach (ParticleSystem system in m_drawingIndicator) {
+						system.Stop();
+					}
 					m_handAnimator.SetBool("IsTriggered", false);
 					Game.EventService.SendMessage(new CastGodsHandMessage(transform.position));
 					m_state = GestureListenerState.Idle;
