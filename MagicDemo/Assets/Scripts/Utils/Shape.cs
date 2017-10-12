033bfec9f4ab1c61ab6e7cb55a7a3f56312d93de
eugene.lagutkin <eugene.lagutkin@gmail.com>
2017-10-12 23:46:11 +0300

    recognizing circle, line, s-symbol. saving/reading symbols from file.

---
+++ MagicDemo/Assets/Scripts/Utils/Shape.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape {
+	public List<Vector3> Points;
+
+	public Shape(List<Vector3> points) {
+		Points = points;
+	}
+
+
+}
--- MagicDemo/Assets/Scripts/Utils/ShapeRecognizer.cs
+++ MagicDemo/Assets/Scripts/Utils/ShapeRecognizer.cs
@@ -1,49 +1,57 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class ShapeRecognizer {
+	private static Shape m_circle;
+	private static Shape m_line;
+	private static Shape m_s;
+
+	static ShapeRecognizer() {
+		m_s = LoadShape("S");
+		m_circle = LoadShape("O");
+		m_line = LoadShape("I");
+	}
 
 	public static float Analyze(List<Vector3> points) {
-		Vector3 center = CalculateCenter(points);
-		//todo: not maxdist, but average dist from center to points
-		//todo: change player points positions, not sample
-		List<Vector3> circle = CreateCircle(center, MaxDist(points) / 2);
-		float result = AnalyzeShape(new List<Vector3>(points), new List<Vector3>(circle)) / MaxDist(points);
-		Debug.Log("Circle: " + result);
-		return result;
+		ResizeShape(points);
+//		WriteDataToFile(JsonUtility.ToJson(new Shape(points)));
+//		foreach (Vector3 point in points) {
+//			Game.EventService.SendMessage(new DebugSpellTrackerMessage(DebugSpellTrackerForm.Sphere, point, Quaternion.identity, 0.1f));
+//		}
+		float resultO = AnalyzeShape(new List<Vector3>(points), new List<Vector3>(m_circle.Points));
+		float resultS = AnalyzeShape(new List<Vector3>(points), new List<Vector3>(m_s.Points));
+		float resultI = AnalyzeShape(new List<Vector3>(points), new List<Vector3>(m_line.Points));
+		Debug.Log("Circle: " + resultO);
+		Debug.Log("Line: " + resultI);
+		Debug.Log("S: " + resultS);
+		return resultO;
 	}
 
-	private static Vector3 CalculateCenter(List<Vector3> points) {
-		Vector3 center = new Vector3();
+
+	private static void ResizeShape(List<Vector3> points) {
+		Vector2 size = GetShapeWH(points);
+		float maxSize = Mathf.Max(size.x, size.y);
 		for (int i = 0; i != points.Count; i++) {
-			center += points[i];
+			points[i] = points[i] * (10 / maxSize);
 		}
-		center /= points.Count;
-		return center;
 	}
 
-	private static List<Vector3> CreateCircle(Vector3 center, float radius) {
-		List<Vector3> circle = new List<Vector3>(128);
-		for (int i = 0; i != 128; i++) {
-			float angle = 360 / 128f * i;
-			Vector3 position = new Vector3(center.x + Mathf.Cos(angle * Mathf.PI / 180) * radius, center.y + Mathf.Sin(angle * Mathf.PI / 180) * radius, center.z);
-			circle.Add(position);
-		}
-		return circle;
-	}
+	private static Vector2 GetShapeWH(List<Vector3> points) {
+		float leftX = float.MaxValue;
+		float rightX = float.MinValue;
+		float topY = float.MinValue;
+		float bottomY = float.MaxValue;
 
-	private static float MaxDist(List<Vector3> shape) {
-		float max = 0;
-		for (int i = 0; i != shape.Count; i++) {
-			for (int g = 0; g != shape.Count; g++) {
-				float d = Vector3.Distance(shape[i], shape[g]);
-				if (d > max) {
-					max = d;
-				}
-			}
+		foreach (Vector3 point in points) {
+			leftX = Mathf.Min(leftX, point.x);
+			rightX = Mathf.Max(rightX, point.x);
+			topY = Mathf.Max(topY, point.y);
+			bottomY = Mathf.Min(bottomY, point.y);
 		}
-		return max;
-	}
+
+		return new Vector2(Mathf.Abs(rightX - leftX), Mathf.Abs(topY - bottomY));
+	} 
 
 	private static float AnalyzeShape(List<Vector3> shape, List<Vector3> example) {
 		List<float> distances  = new List<float>(32);
@@ -67,6 +75,20 @@ public class ShapeRecognizer {
 		}
 		sq /= Mathf.Sqrt(distances.Count);
 		return sq;
+	}
+
+	public static void WriteDataToFile(string jsonString) {
+		string path = Application.dataPath + "/Levels.json";
+		File.WriteAllText(path, jsonString);
+#if UNITY_EDITOR
+		UnityEditor.AssetDatabase.Refresh ();
+#endif
+	}
+
+	private static Shape LoadShape(string name) {
+		TextAsset settingsJson = Resources.Load<TextAsset>(name);
+		Shape shape = JsonUtility.FromJson<Shape>(settingsJson.text);
+		return shape;
 	}
 
 }
