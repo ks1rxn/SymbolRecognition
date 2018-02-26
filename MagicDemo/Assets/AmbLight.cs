21bf20c60593adb763042f405fa112b8a0bcd7a4
Pandemonium\Gigamesh <serkuz09@gmail.com>
2018-02-26 15:35:06 +0100

    4 scenes, amazing content!

---
+++ MagicDemo/Assets/AmbLight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbLight : MonoBehaviour {
+
+	// Use this for initialization
+
+
+    public bool setAmbientLight;
+    public float ambientLight;
+
+
+	void Update() {
+		{
+            RenderSettings.ambientIntensity = this.ambientLight;
+        }
+    }
+ 
+}
